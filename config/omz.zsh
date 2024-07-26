4f1bad3f9d36f10b126b741f2c8831cd6a05fcc5
wqsrandy <15620601349@163.com>
2024-07-26 13:55:25 +0800

    INIT

---
+++ config/omz.zsh
@@ -0,0 +1,4 @@
+source $OMZ/lib/omz.sh
+source $OMZ/plugins/fzf-tab/fzf-tab.zsh
+source $OMZ/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+source $OMZ/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
